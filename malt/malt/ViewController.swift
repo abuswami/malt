c207f216e94922e1f5a0ea916b39473cd0331809
Abu Swaminathan <abuswami@gmail.com>
2019-07-14 15:10:37 -0400

    first commit

---
+++ malt/malt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  malt
+//
+//  Created by Abu Swaminathan on 7/14/19.
+//  Copyright © 2019 abuswaminathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
