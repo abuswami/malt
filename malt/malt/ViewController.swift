cde02f712e0dde5e4384024fa393a9a3b6e7c1ec
Vijay Iyer <vijayiyer97@gmail.com>
2019-07-14 17:50:28 -0400

    Update malt
    * Added Firestone pod
    * Added test for Firebase db funcitonality

--- malt/malt/ViewController.swift
+++ malt/malt/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import FirebaseFirestore
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib
+        
+        let db = Firestore.firestore()
+        db.collection("beers").getDocuments { (snapshot, error)  in
+            
+            if error != nil {
+                print(error)
+            } else {
+                for document in (snapshot?.documents)! {
+                    print(document.data()["beer_type"] as? String)
+                }
+            }
+        }
     }
-
-
 }
-
